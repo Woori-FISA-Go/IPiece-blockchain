4d21b7f61abd988c19353268fd8396e615f6aef7
Gill010147 <qudrlftm0104@gmail.com>
2025-11-08 06:27:12 +0000

    [#3, #4] feat: SecurityToken 및 KRWT 핵심 컨트랙트 추가

---
+++ contracts/src/KRWT.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract KRWT is ERC20, Ownable {
+    constructor() ERC20("Korean Won Token", "KRWT") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyOwner {
+        _burn(from, amount);
+    }
+}
---
+++ contracts/test/KRWT.t.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "forge-std/Test.sol";
+import "../src/KRWT.sol";
+
+contract KRWTTest is Test {
+    KRWT krwt;
+    address owner = address(1);
+    address user1 = address(2);
+    address user2 = address(3);
+    
+    function setUp() public {
+        vm.startPrank(owner);
+        krwt = new KRWT();
+        vm.stopPrank();
+    }
+    
+    function test_InitialState() public {
+        assertEq(krwt.name(), "Korean Won Token", "Token name mismatch");
+        assertEq(krwt.symbol(), "KRWT", "Token symbol mismatch");
+        assertEq(krwt.owner(), owner, "Owner mismatch");
+        assertEq(krwt.totalSupply(), 0, "Initial total supply should be 0");
+    }
+    
+    function test_Mint() public {
+        vm.startPrank(owner);
+        krwt.mint(user1, 1000 * 10 ** 18);
+        vm.stopPrank();
+
+        assertEq(krwt.balanceOf(user1), 1000 * 10 ** 18, "User1 balance after mint mismatch");
+        assertEq(krwt.totalSupply(), 1000 * 10 ** 18, "Total supply after mint mismatch");
+    }
+
+    function test_RevertWhen_Mint_NotOwner() public {
+        vm.startPrank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+        krwt.mint(user2, 100 * 10 ** 18);
+        vm.stopPrank();
+    }
+
+    function test_Burn() public {
+        vm.startPrank(owner);
+        krwt.mint(user1, 1000 * 10 ** 18);
+        krwt.burn(user1, 300 * 10 ** 18);
+        vm.stopPrank();
+
+        assertEq(krwt.balanceOf(user1), 700 * 10 ** 18, "User1 balance after burn mismatch");
+        assertEq(krwt.totalSupply(), 700 * 10 ** 18, "Total supply after burn mismatch");
+    }
+
+    function test_RevertWhen_Burn_NotOwner() public {
+        vm.startPrank(owner);
+        krwt.mint(user1, 1000 * 10 ** 18);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+        krwt.burn(user1, 100 * 10 ** 18);
+        vm.stopPrank();
+    }
+
+    function test_Transfer() public {
+        vm.startPrank(owner);
+        krwt.mint(user1, 1000 * 10 ** 18);
+        vm.stopPrank();
+
+        vm.startPrank(user1);
+        krwt.transfer(user2, 200 * 10 ** 18);
+        vm.stopPrank();
+
+        assertEq(krwt.balanceOf(user1), 800 * 10 ** 18, "User1 balance after transfer mismatch");
+        assertEq(krwt.balanceOf(user2), 200 * 10 ** 18, "User2 balance after transfer mismatch");
+    }
+}
